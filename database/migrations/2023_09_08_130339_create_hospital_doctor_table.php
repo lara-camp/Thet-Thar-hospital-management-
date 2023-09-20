e5ca0ebaafb0f76dfeda7eeba5882f8d3cb0f5e6
MinHeinKoKo <kokominhein96@gmail.com>
2023-09-20 21:23:59 +0630

    hospital update

--- database/migrations/2023_09_08_130339_create_hospital_doctor_table.php
+++ database/migrations/2023_09_08_130339_create_hospital_doctor_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('hospital_id');
             $table->bigInteger('doctor_id');
-            $table->time('checkin_time');
+            $table->time('checkin_time')->nullable();
             $table->timestamps();
         });
     }
