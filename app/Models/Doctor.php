d97484eb9824a8f755d8a6ccc2f90e5b87b7fc0e
LwinMoeAung <53222794+lwinmoeaung-springarts@users.noreply.github.com>
2023-09-22 11:12:11 +0630

    fix bugs

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -18,7 +18,7 @@ class Doctor extends Model
 
     public function hospitals()
     {
-        return $this->belongsToMany(Hospital::class, 'hospital_doctor', 'doctor_id', 'hospital_id');
+        return $this->belongsToMany(Hospital::class, 'hospital_doctors', 'doctor_id', 'hospital_id');
     }
 
     public function department()
