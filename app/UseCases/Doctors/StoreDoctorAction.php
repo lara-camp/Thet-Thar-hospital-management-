3e418c565afd9ae6f36827efe6104f43eae72021
LwinMoeAung <53222794+lwinmoeaung-springarts@users.noreply.github.com>
2023-09-27 10:07:17 +0900

    add doctor appointment time

--- app/UseCases/Doctors/StoreDoctorAction.php
+++ app/UseCases/Doctors/StoreDoctorAction.php
@@ -9,11 +9,32 @@ class StoreDoctorAction
 {
     public function __invoke($formData): int
     {
-        $fileName = FileHelper::fileMover($formData['image']);
         $doctor = Doctor::create($formData);
-        $doctor->images()->create([
-            'url' =>  config('folderName') . '/' . $fileName, // Adjust the path as needed
-        ]);
+
+        if (request()->has('image')) {
+            $fileName = FileHelper::fileMover($formData['image']);
+            $doctor->images()->create([
+                'url' =>  config('folderName') . '/' . $fileName, // Adjust the path as needed
+            ]);
+        }
+        // Create appointment times based on duty start and end times
+        $dutyStartTime = \Carbon\Carbon::parse($formData['duty_start_time']); // Assuming 'duty_start_time' is in a valid time format
+        $dutyEndTime = \Carbon\Carbon::parse($formData['duty_end_time']); // Assuming 'duty_end_time' is in a valid time format
+
+        $interval = 30; // 30 minutes interval, you can adjust this as needed
+        $appointmentTime = $dutyStartTime->copy();
+
+        while ($appointmentTime <= $dutyEndTime) {
+            // Create an appointment entry in the appointment_time table
+            $doctor->appointmentTimes()->create([
+                'doctor_id' => $doctor->id,
+                'appointment_time' => $appointmentTime->format('H:i'), // Format time as 'HH:mm'
+            ]);
+
+            // Increment the appointment time by the specified interval
+            $appointmentTime->addMinutes($interval);
+        }
+
         return 201;
     }
 }
