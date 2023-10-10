87342b66528b4e1d2e51c9596bece2a8ea5f0b58
MinHeinKoKo <kokominhein96@gmail.com>
2023-10-10 13:53:16 +0630

    make some change

--- app/Http/Resources/AppointmentResource.php
+++ app/Http/Resources/AppointmentResource.php
@@ -14,29 +14,29 @@ class AppointmentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $uri = $request->route()->uri;
-        if(!$uri) return [];
-
-        if($uri === 'api/today-appointment'){
-            return [
-                'id'      => $this->id,
-                'patientId'    => $this->patient->userInfo->id ?? null,
-                'doctorId'    => $this->doctor_id ?? null,
-                'patientName'    => $this->patient->userInfo->name ?? null,
-                'doctorName'   => $this->doctor->userInfo->name ?? null,
-                'bookingId' => $this->booking_id ?? null,
-                'doctorLocation' => $this->doctor->hospitals->pluck('location') ?? null,
-                'appointmentTime'    => $this->appointment_time ?? null,
-                'appointmentDate'    => $this->appointment_date ?? null,
-                'is_visible'  => $this->is_visible ?? null,
-                'description'  => $this->description ?? null,
-                'status'  => $this->status ?? null,
-                'appointmentType'  => $this->appointment_type ?? null,
-            ];
-        }
+//        $uri = $request->route()->uri;
+//        if(!$uri) return [];
+//
+//        if($uri === 'api/today-appointment'){
+//            return [
+//                'id'      => $this->id,
+//                'patientId'    => $this->patient->userInfo->id ?? null,
+//                'doctorId'    => $this->doctor_id ?? null,
+//                'patientName'    => $this->patient->userInfo->name ?? null,
+//                'doctorName'   => $this->doctor->userInfo->name ?? null,
+//                'bookingId' => $this->booking_id ?? null,
+//                'doctorLocation' => $this->doctor->hospitals->pluck('location') ?? null,
+//                'appointmentTime'    => $this->appointment_time ?? null,
+//                'appointmentDate'    => $this->appointment_date ?? null,
+//                'is_visible'  => $this->is_visible ?? null,
+//                'description'  => $this->description ?? null,
+//                'status'  => $this->status ?? null,
+//                'appointmentType'  => $this->appointment_type ?? null,
+//            ];
+//        }
         return [
             'id'      => $this->id,
-            'patientId'    => $this->patient_id ?? null,
+            'patientId'    => $this->patient->userInfo->id ?? null,
             'doctorId'    => $this->doctor_id ?? null,
             'patientName'    => $this->patient->userInfo->name ?? null,
             'doctorName'   => $this->doctor->userInfo->name ?? null,
