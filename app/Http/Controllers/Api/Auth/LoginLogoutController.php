0f57ac43774079ad00652fb84b7893d9f380643d
LwinMoeAung <53222794+lwinmoeaung-springarts@users.noreply.github.com>
2023-10-15 00:05:11 +0900

    fix bugs

--- app/Http/Controllers/Api/Auth/LoginLogoutController.php
+++ app/Http/Controllers/Api/Auth/LoginLogoutController.php
@@ -7,21 +7,53 @@ use App\Traits\HttpResponses;
 use Illuminate\Http\JsonResponse;
 use App\UseCases\Auth\LoginAction;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LoginLogoutController extends Controller
 {
     use HttpResponses;
 
     //Login Method
-    public function login(Request $request): JsonResponse
+    public function login(Request $request)
     {
-        $result = (new LoginAction)($request);
-        return response()->json($result);
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required|min:6',
+            ]);
+
+            if ($validator->fails()) {
+                return $this->error($validator->errors()->first(), null, 422);
+            }
+
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                $user = auth()->user();
+                $token = $user->createToken('myapptoken');
+                return $this->success('Successfully logged in.', [
+                    'token' => $token->plainTextToken,
+                    'user' => [
+                        'id' => $user->id,
+                        'name' => $user->name,
+                        'email' => $user->email,
+                        'phone' => $user->phone,
+                        'role' => $user->role,
+                    ]
+                ], 200);
+            }
+
+            return $this->error('Your credentials is not correct.', null, 422);
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), null, 500);
+        }
     }
 
     //Logout Method
     public function logout(): JsonResponse
     {
-        return (new LogoutAction)();
+        $user = auth()->user();
+        $user->tokens()->delete();
+        return $this->success('Successfully logged out.', null, 200);
     }
 }
