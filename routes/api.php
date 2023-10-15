cf5f54e9eb2e62a24f42da5c47fae38a29e5d565
aungzawphyo <aungzawphyo1102@gmail.com>
2023-10-16 00:14:21 +0630

    doctor main features api

--- routes/api.php
+++ routes/api.php
@@ -54,6 +54,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/dashboard/hospital/{hospitalId}', [HospitalController::class, 'dashboardData']);
     Route::put('/dashboard/{hospitalId}/head', [HospitalController::class, 'updateHead']);
     Route::get('/fetch-hospital', [UserController::class, 'fetchUserHospital']); //fetch the user of hospital if user is HospitalAdmin
+
+    // Doctor Dashboard
+    Route::get('/dashboard/doctor/{doctor}/hospitals', [DoctorController::class, 'hospitals']);
+    Route::get('/dashboard/doctor/{doctor}/patients', [DoctorController::class, 'patients']);
+    Route::post('/dashboard/doctor/{doctor}/update', [DoctorController::class, 'updateProfile']);
+    Route::get('/dashboard/doctor/{doctor}/appointments', [DoctorController::class, 'appointments']);
 });
 
 
