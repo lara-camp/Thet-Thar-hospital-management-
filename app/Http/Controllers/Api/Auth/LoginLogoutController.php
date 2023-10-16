7edf80d8238aafec3f4afc0e5f94ee760dab8cbb
MinHeinKoKo <kokominhein96@gmail.com>
2023-10-16 08:51:03 +0630

    adding some

--- app/Http/Controllers/Api/Auth/LoginLogoutController.php
+++ app/Http/Controllers/Api/Auth/LoginLogoutController.php
@@ -6,15 +6,16 @@ use App\Traits\HttpResponses;
 use Illuminate\Http\JsonResponse;
 use App\UseCases\Auth\LoginAction;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class LoginLogoutController extends Controller
 {
     use HttpResponses;
 
     //Login Method
-    public function login(): JsonResponse
+    public function login(Request $request): JsonResponse
     {
-        $result = (new LoginAction)();
+        $result = (new LoginAction)($request->all);
         return response()->json($result);
     }
 
