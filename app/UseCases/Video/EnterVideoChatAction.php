8de32b985374d009bff82590f6f075c279fb0733
MinHeinKoKo <kokominhein96@gmail.com>
2023-10-25 14:19:35 +0630

    create and validate video

---
+++ app/UseCases/Video/EnterVideoChatAction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\UseCases\Video;
+
+use App\Models\Appointment;
+use App\Models\Doctor;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+
+class EnterVideoChatAction
+{
+    public function __invoke($bookingId)
+    {
+        $METERED_DOMAIN = env('METERED_DOMAIN');
+        $METERED_SECRET_KEY = env('METERED_SECRET_KEY');
+        if ($this->checkRole($bookingId)) {
+            $response = Http::get("https://{$METERED_DOMAIN}/api/v1/room/{$bookingId}?secretKey={$METERED_SECRET_KEY}");
+
+            $roomName = $response->json("roomName");
+            return $roomName;
+        }
+        return null;
+    }
+    private function checkRole($bookingId)
+    {
+        $role = Auth::user()->role;
+        if ($role === 'doctor') {
+            $doctor = Doctor::where('user_id', Auth::id())->first();
+            $id = $doctor->id;
+            return $this->isExists("doctor_id", $id, $bookingId);
+        } else {
+            return $this->isExists("patient_id", Auth::id(), $bookingId);
+        }
+    }
+
+    private function isExists($column, $id, $bookingId)
+    {
+
+        return Appointment::where($column, $id)->where('booking_id', $bookingId)->where('appointment_type', 'video')->exists();
+    }
+}
